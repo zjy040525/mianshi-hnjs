e874ea5c34ab7a0391bc58ff7e8f7c3878f36086
Jia-Yao Zhao <63852433+zjy040525@users.noreply.github.com>
2023-06-18 13:12:26 +0800

    init

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const result = require('@/util/result');
 const { SHA256 } = require('crypto-js');
 const { engine } = require('express-handlebars');
 const path = require('path');
@@ -35,17 +34,9 @@ class CloudBaseRunServer {
     );
     this.express.use(cors());
     this.express.use(logger);
-    // 用于判断和加密用户密码的中间件
+    // 用于加密用户密码的中间件
     this.express.use((req, res, next) => {
       if (req.body.password) {
-        if (req.body.password.length < 8) {
-          res.status(400).json(result(400, null, '密码长度不得少于8个字符！'));
-          return;
-        }
-        if (req.body.password.length > 32) {
-          res.status(400).json(result(400, null, '密码长度不得大于32个字符！'));
-          return;
-        }
         req.body.password = SHA256(req.body.password).toString();
       }
       next();
