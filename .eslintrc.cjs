8880674fabd0bc087dd8f3d7b7d44407998f6528
Jia-Yao Zhao <63852433+zjy040525@users.noreply.github.com>
2023-06-24 15:40:47 +0800

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,14 @@ const { defineConfig } = require('eslint-define-config');
 module.exports = defineConfig({
   env: {
     browser: true,
-    es2021: true,
+    es2022: true,
     node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
@@ -17,10 +18,11 @@ module.exports = defineConfig({
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['@typescript-eslint', 'react', 'react-refresh'],
   rules: {
     '@typescript-eslint/no-var-requires': 'off',
     'react/react-in-jsx-scope': 'off',
+    'react-refresh/only-export-components': 'warn',
   },
   settings: {
     react: {
