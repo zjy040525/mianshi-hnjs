fa571aae2253ed86781e6f13d8fd9a18ac91fd65
Jia-Yao Zhao <63852433+zjy040525@users.noreply.github.com>
2023-06-25 14:19:28 +0800

    init

--- src/atoms/auth.ts
+++ src/atoms/auth.ts
@@ -7,28 +7,28 @@ const { persistAtom } = recoilPersist();
 export const idStateAtom = atom<number | null>({
   key: 'idStateAtom',
   effects_UNSTABLE: [persistAtom],
-  default: undefined,
+  default: null,
 });
 
 export const tokenStateAtom = atom<string | null>({
   key: 'tokenStateAtom',
   effects_UNSTABLE: [persistAtom],
-  default: undefined,
+  default: null,
 });
 
 export const usernameStateAtom = atom<string | null>({
   key: 'usernameStateAtom',
   effects_UNSTABLE: [persistAtom],
-  default: undefined,
+  default: null,
 });
 
 export const nicknameStateAtom = atom<string | null>({
   key: 'nicknameStateAtom',
   effects_UNSTABLE: [persistAtom],
-  default: undefined,
+  default: null,
 });
 
 export const permissionStateAtom = atom<Permission | null>({
   key: 'permissionStateAtom',
-  default: undefined,
+  default: null,
 });
