f302270f29319c04a96a37eeab1b299ae3178837
Jia-Yao Zhao <63852433+zjy040525@users.noreply.github.com>
2023-07-24 13:38:01 +0800

    perf: page loading

---
+++ src/components/PageLoading/index.tsx
@@ -0,0 +1,16 @@
+import { Spin } from 'antd';
+import { FC } from 'react';
+import classes from './index.module.less';
+
+/**
+ * 页面中组件加载
+ *
+ * @author Jia-Yao Zhao
+ */
+export const PageLoading: FC = () => {
+  return (
+    <div className={classes.chunkLoading}>
+      <Spin />
+    </div>
+  );
+};
