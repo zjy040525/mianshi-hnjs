96353c261733118db6b9e375f535fdcba7f0e758
Jia-Yao Zhao <63852433+zjy040525@users.noreply.github.com>
2023-08-24 16:02:18 +0800

    fix: remove @ant-design/happy-work-theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { HappyProvider } from '@ant-design/happy-work-theme';
 import { App as AppProvider, ConfigProvider } from 'antd';
 import 'antd/dist/reset.css';
 import locale from 'antd/locale/zh_CN';
@@ -20,15 +19,13 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <RecoilRoot>
       <HelmetProvider>
         <ConfigProvider locale={locale}>
-          <HappyProvider>
-            <AppProvider notification={{ maxCount: 5 }}>
-              <AuthorizationGuard>
-                <React.Suspense fallback={<GlobalLoading />}>
-                  <RouterProvider router={createBrowserRouter(routes)} />
-                </React.Suspense>
-              </AuthorizationGuard>
-            </AppProvider>
-          </HappyProvider>
+          <AppProvider notification={{ maxCount: 5 }}>
+            <AuthorizationGuard>
+              <React.Suspense fallback={<GlobalLoading />}>
+                <RouterProvider router={createBrowserRouter(routes)} />
+              </React.Suspense>
+            </AuthorizationGuard>
+          </AppProvider>
         </ConfigProvider>
       </HelmetProvider>
     </RecoilRoot>
